ef92ab2dabb17ca728838cf8c871d78e8ff08fc3
Foucsi <julien.foucartreact@gmail.com>
2023-05-24 19:26:08 +0200

    add model user and connection

---
+++ models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    // You should hash passwords before saving to DB
+    type: String,
+    required: true,
+  },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  uniqueId: { type: String, unique: true }, //token uuid4
+});
+
+module.exports = mongoose.model("users", UserSchema);
