dcac07e0d6189ac54484b3e095fd08bc76745319
Foucsi <julien.foucartreact@gmail.com>
2023-05-24 19:30:29 +0200

    add rte signin

---
+++ routes/signin.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router = express.Router();
+const { checkBody } = require("../modules/checkBody");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { ErrorMessages, HttpStatus } = require("../errors/error_messages");
+
+const User = require("../models/users");
+
+router.post("/login", async (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    return res.json({ result: false, error: ErrorMessages.MISSING_FIELDS });
+  }
+  try {
+    const data = await User.findOne({ username: req.body.username });
+    if (data && (await bcrypt.compare(req.body.password, data.password))) {
+      // Generate a JWT token for the user
+      const token = jwt.sign({ userId: data._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      return res.json({ result: true, user: data, token });
+    } else {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ result: false, error: ErrorMessages.USER_NOT_FOUND });
+    }
+  } catch (err) {
+    return res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .json({ result: false, error: err });
+  }
+});
+
+module.exports = router;
