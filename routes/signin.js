1741b154c4f90476c003a6a363d99539b4d4645b
Foucsi <julien.foucartreact@gmail.com>
2023-05-27 15:23:52 +0200

    iptimization rte login

--- routes/signin.js
+++ routes/signin.js
@@ -25,9 +25,10 @@ router.post("/login", async (req, res) => {
         .json({ result: false, error: ErrorMessages.USER_NOT_FOUND });
     }
   } catch (err) {
+    console.log(err);
     return res
       .status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .json({ result: false, error: err });
+      .json({ result: false, error: "Server internal error" });
   }
 });
 
