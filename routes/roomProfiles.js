f576b275f583a1f92c7dde15bdd81971fb5d2115
Nooman Miled <noon.supply@gmail.com>
2023-06-09 09:07:56 +0200

    adding ajouter room route

--- routes/roomProfiles.js
+++ routes/roomProfiles.js
@@ -14,6 +14,13 @@ router.post('/ajouter-room', async (req, res) => {
       return res.status(404).json({ error: 'La room n\'existe pas' });
     }
 
+    // Vérifier si le RoomProfile existe déjà pour cet utilisateur
+    const existingProfile = await RoomProfile.findOne({ roomId, profileId: userId });
+
+    if (existingProfile) {
+      return res.status(409).json({ error: 'Utilisateur déjà présent dans la room' });
+    }
+
     // Calculer la date et heure de validité
     const validity = new Date();
     validity.setHours(validity.getHours() + 5);
@@ -28,12 +35,13 @@ router.post('/ajouter-room', async (req, res) => {
     // Enregistrer le RoomProfile dans la base de données
     await roomProfile.save();
 
-    return res.status(200).json({ message: 'Room ajoutée avec succès' });
+    return res.status(200).json({ message: 'Utilisater ajoutée avec succès dans la room' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Erreur lors de l\'ajout de la room' });
   }
 });
+
 
   
 
