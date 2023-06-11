7ee582bb8621b02e4fe92e10626b02b833ff9a7d
Foucsi <julien.foucartreact@gmail.com>
2023-06-11 13:22:58 +0200

    add signin rte

--- routes/signinAdmin.js
+++ routes/signinAdmin.js
@@ -5,20 +5,20 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { ErrorMessages, HttpStatus } = require("../errors/error_messages");
 
-const User = require("../models/users");
+const Admin = require("../models/admins");
 
 router.post("/login", async (req, res) => {
   if (!checkBody(req.body, ["email", "password"])) {
     return res.json({ result: false, error: ErrorMessages.MISSING_FIELDS });
   }
   try {
-    const data = await User.findOne({ email: req.body.email });
+    const data = await Admin.findOne({ email: req.body.email });
     if (data && (await bcrypt.compare(req.body.password, data.password))) {
       // Generate a JWT token for the user
       const token = jwt.sign({ userId: data._id }, process.env.JWT_SECRET, {
         expiresIn: "1h",
       });
-      return res.json({ result: true, user: data, token });
+      return res.json({ result: true, admin: data, token });
     } else {
       return res
         .status(HttpStatus.BAD_REQUEST)
