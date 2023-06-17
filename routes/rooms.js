55f5a872ee1f6dcff58fdde857aa51694ebd8bfc
Foucsi <julien.foucartreact@gmail.com>
2023-06-17 11:04:58 +0200

    add rte rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -3,11 +3,26 @@ const Room = require("../models/rooms");
 const User = require("../models/users");
 const { ErrorMessages, HttpStatus } = require("../errors/error_messages");
 
-
 const router = express.Router();
 
-router.post("/createRooms", async (req, res) => {
+// get all rooms
+router.get("/rooms", async (req, res) => {
+  try {
+    const existingRooms = await Room.find();
+    if (existingRooms === 0) {
+      return res.status(404).json({ message: "Cannot find rooms" });
+    }
+    return res.json({
+      result: true,
+      roomsLength: existingRooms.length,
+      roomName: existingRooms.map((room) => room.roomName),
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
+router.post("/createRooms", async (req, res) => {
   const { roomId, roomName, uniqueId } = req.body;
 
   if (!uniqueId || !roomId || !roomName) {
@@ -22,14 +37,14 @@ router.post("/createRooms", async (req, res) => {
         .status(400)
         .json({ result: false, error: ErrorMessages.USER_NOT_FOUND });
     }
-  const newRoom = new Room({
-    userId: user._id,
-    roomId: req.body.roomId,
-    roomName: req.body.roomName,
-  });
+    const newRoom = new Room({
+      userId: user._id,
+      roomId: req.body.roomId,
+      roomName: req.body.roomName,
+    });
 
     const saveNewRoom = await newRoom.save();
-    return res.json({result: true, saveNewRoom});
+    return res.json({ result: true, saveNewRoom });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
