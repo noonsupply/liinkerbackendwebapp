6f7846b67b3e6dcb9e29e6457268f314201f6ce3
Nooman Miled <noon.supply@gmail.com>
2023-06-19 10:56:21 +0200

    modification in route usercontact

--- routes/usercontacts.js
+++ routes/usercontacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserContact = require('../models/UserContacts');
+const UserContact = require('../models/usercontacts');
 const User = require('../models/users');
 
 
