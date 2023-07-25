2207f01cb39451ae5bf733491f5d153b6ffb5bf3
Nooman Miled <noon.supply@gmail.com>
2023-07-25 11:10:30 +0200

    modification get nearby

--- routes/gpsPosition.js
+++ routes/gpsPosition.js
@@ -4,7 +4,21 @@ const User = require("../models/users");
 const GPSPosition = require('../models/gpsPosition');
 
 router.get('/nearby', async (req, res) => {
-  const { longitude, latitude } = req.query;
+  let { longitude, latitude } = req.query;
+
+  // Vérifiez si la longitude et la latitude sont présentes
+  if (!longitude || !latitude) {
+    return res.status(400).json({ message: 'Les coordonnées de longitude et de latitude sont requises.' });
+  }
+
+  // Essayez de convertir les coordonnées en nombre
+  longitude = parseFloat(longitude);
+  latitude = parseFloat(latitude);
+
+  // Vérifiez si la conversion a réussi
+  if (isNaN(longitude) || isNaN(latitude)) {
+    return res.status(400).json({ message: 'Les coordonnées de longitude et de latitude doivent être des nombres.' });
+  }
 
   const radiusInRadians = 30 / 6371; // Convertir 30 mètres en radians
   const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000); // Calculer l'heure qu'il était il y a 30 minutes
@@ -28,6 +42,8 @@ router.get('/nearby', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+
 
 
 router.put('/position/:id/gps', async (req, res) => {
