f8ea54f39582cecf107620a2c2b99b7957260c36
Nooman Miled <noon.supply@gmail.com>
2023-10-13 14:07:34 +0200

    modification route signin

--- routes/signin.js
+++ routes/signin.js
@@ -4,22 +4,24 @@ const { checkBody } = require("../modules/checkBody");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { ErrorMessages, HttpStatus } = require("../errors/error_messages");
-
+const Role = require("../models/roles");
 const User = require("../models/users");
 
 router.post("/login", async (req, res) => {
-
   if (!checkBody(req.body, ["email", "password"])) {
     return res.json({ result: false, error: ErrorMessages.MISSING_FIELDS });
   }
   try {
-    const data = await User.findOne({ email: req.body.email });
-    if (data && (await bcrypt.compare(req.body.password, data.password))) {
-      // Generate a JWT token for the user
-      const token = jwt.sign({ userId: data._id }, process.env.JWT_SECRET, {
+    const user = await User.findOne({ email: req.body.email });
+    if (user && (await bcrypt.compare(req.body.password, user.password))) {
+      // L'utilisateur est authentifié, maintenant nous récupérons le rôle.
+      const role = await Role.findOne({ userId: user._id });
+      // Générer un JWT pour l'utilisateur
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
         expiresIn: "1h",
       });
-      return res.json({ result: true, user: data, token });
+
+      return res.json({ result: true, user, token, role: role ? role.role : 'No role assigned' });
     } else {
       return res
         .status(HttpStatus.BAD_REQUEST)
