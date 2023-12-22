c0638d0547e7b693716d1ebc1a6efe52197e7d14
Nooman Miled <noon.supply@gmail.com>
2023-12-22 21:11:06 +0100

    adding display car userid

--- routes/v1/profils.js
+++ routes/v1/profils.js
@@ -182,6 +182,25 @@ router.get("/displayPublicCards", async (req, res) => {
 }
 );
 
+router.get("/displayPublicCards/:userId", async (req, res) => {
+
+  const { profilId } = req.params;
+  
+  try {
+    const profiles = await Profil.find({ profilId, isPublic: true });
+    if (!profiles.length) {
+      return res.status(400).json({ result: false, error: ErrorMessages.NOT_CARD_FOR_USER });
+    }
+
+    return res.json({ result: true, profiles });
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json({ result: false, error: HttpStatus.INTERNAL_SERVER_ERROR });
+  }
+}
+);
+
 
 
 module.exports = router;
